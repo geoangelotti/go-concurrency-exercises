cc406631863e1c557d6933ceea2f46ddb873f582
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-07-30 12:14:24 +0200

    section 2 exercises

--- 01-exercise/01-goroutines/01-hello/main.go
+++ 01-exercise/01-goroutines/01-hello/main.go
@@ -19,12 +19,19 @@ func main() {
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
+	go fun("goroutine call")
 
 	// goroutine with anonymous function
+	go func(s string) {
+		fun(s)
+	}("anonymous call")
 
 	// goroutine with function value call
+	value := fun
+	go value("function value call")
 
 	// wait for goroutines to end
+	time.Sleep(time.Second / 3)
 
 	fmt.Println("done..")
 }
--- 01-exercise/01-goroutines/03-join/main.go
+++ 01-exercise/01-goroutines/03-join/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -11,10 +12,15 @@ func main() {
 
 	var data int
 
+	var wg sync.WaitGroup
+
+	wg.Add(1)
 	go func() {
 		data++
+		defer wg.Done()
 	}()
 
+	wg.Wait()
 	fmt.Printf("the value of data is %v\n", data)
 
 	fmt.Println("Done..")
