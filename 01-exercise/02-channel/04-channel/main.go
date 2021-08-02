6b32486e9a628335d510ca7ac990d61a4c5a690d
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-02 12:59:53 +0200

    section 3 exercises

--- 01-exercise/02-channel/04-channel/main.go
+++ 01-exercise/02-channel/04-channel/main.go
@@ -1,20 +1,30 @@
 package main
 
+import "fmt"
+
 // TODO: Implement relaying of message with Channel Direction
 
-func genMsg() {
+func genMsg(ch chan<- string) {
 	// send message on ch1
+	ch <- "hello"
 }
 
-func relayMsg() {
+func relayMsg(in <-chan string, out chan<- string) {
 	// recv message on ch1
+	r := <-in
 	// send it on ch2
+	out <- r
 }
 
 func main() {
 	// create ch1 and ch2
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
 	// spine goroutine genMsg and relayMsg
-
+	go genMsg(ch1)
+	go relayMsg(ch1, ch2)
 	// recv message on ch2
+	r := <-ch2
+	fmt.Println(r)
 }
